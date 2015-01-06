cbbd95a39aa09b77a7dab656b5f8dd3e1095ef57
leonzhang1109 <leonzhang1109@hotmail.com>
2015-01-06 11:25:29 -0800

    Update and rename instagram_client.js to wechat_client.js

--- wechat_client.js
+++ wechat_client.js
@@ -6,27 +6,27 @@ Instagram.requestCredential = function (options, credentialRequestCompleteCallba
     options = {};
   }
 
-  var config = ServiceConfiguration.configurations.findOne({service: 'instagram'});
+  var config = ServiceConfiguration.configurations.findOne({service: 'wechat'});
   if (!config) {
     credentialRequestCompleteCallback && credentialRequestCompleteCallback(
       new ServiceConfiguration.ConfigError());
     return;
   }
   var credentialToken = Random.secret();
-  var loginStyle = OAuth._loginStyle('instagram', config, options);
+  var loginStyle = OAuth._loginStyle('wechat', config, options);
   var scope = (options && options.requestPermissions) || ['basic', 'likes', 'relationships', 'comments'];
   var flatScope = _.map(scope, encodeURIComponent).join('+');
 
   var loginUrl =
-    'https://instagram.com/oauth/authorize' +
-      '?client_id=' + config.clientId +
+    'https://open.weixin.qq.com/connect/oauth2/authorize' +
+      '?appid=' + config.clientId +
       '&response_type=code' +
       '&scope=' + flatScope +
-      '&redirect_uri=' + OAuth._redirectUri('instagram', config) +
+      '&redirect_uri=' + OAuth._redirectUri('wechat', config) +
       '&state=' + OAuth._stateParam(loginStyle, credentialToken);
 
   OAuth.launchLogin({
-    loginService: "instagram"
+    loginService: "wechat"
     , loginStyle: loginStyle
     , loginUrl: loginUrl
     , credentialRequestCompleteCallback: credentialRequestCompleteCallback
