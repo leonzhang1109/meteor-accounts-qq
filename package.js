57bd56ab13582a11783ed0d424f095e22572cc51
leonzhang1109 <leonzhang1109@hotmail.com>
2015-01-07 11:17:30 -0800

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "QQ account login for meteor",
-  "version": "0.0.1",
+  "version": "0.0.2",
   "git": "https://github.com/leonzhang1109/meteor-accounts-qq",
   "name": "leonzhang1109:accounts-qq"
 });
