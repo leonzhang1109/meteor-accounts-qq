08d510194b178c4742bbbfd3b61aca639a35a6d1
leonzhang1109 <leonzhang1109@hotmail.com>
2015-01-07 10:20:09 -0800

    Update and rename wechat_server.js to qq_server.js

--- qq_server.js
+++ qq_server.js
@@ -1,6 +1,6 @@
-Wechat = {};
+QQ = {};
 
-Oauth.registerService('wechat', 2, null, function(query) {
+Oauth.registerService('qq', 2, null, function(query) {
 
   var response = getTokenResponse(query);
   var accessToken = response.access_token;
@@ -12,13 +12,13 @@ Oauth.registerService('wechat', 2, null, function(query) {
     serviceData: serviceData,
     options: {
       profile: { name: identity.full_name },
-      services: { wechat: identity }
+      services: { qq: identity }
     }
   };
 });
 
 var getTokenResponse = function (query) {
-  var config = ServiceConfiguration.configurations.findOne({service: 'wechat'});
+  var config = ServiceConfiguration.configurations.findOne({service: 'qq'});
 
   if (!config)
     throw new ServiceConfiguration.ConfigError();
@@ -26,12 +26,12 @@ var getTokenResponse = function (query) {
   var response;
   try {
     response = HTTP.post(
-      "https://api.wechat.com/cgi-bin/token", {
+      "https://graph.qq.com/oauth2.0/token", {
         params: {
           code: query.code,
-          appid: config.appId,
-          redirect_uri: OAuth._redirectUri("wechat", config),
-          secret: OAuth.openSecret(config.secret),
+          client_id: config.clientId,
+          redirect_uri: OAuth._redirectUri("qq", config),
+          client_secret: OAuth.openSecret(config.secret),
           grant_type: 'authorization_code'
         }
       });
@@ -43,13 +43,13 @@ var getTokenResponse = function (query) {
     if (response.content.error)
         throw response.content;
   } catch (err) {
-    throw _.extend(new Error("Failed to complete OAuth handshake with Wechat. " + err.message),
+    throw _.extend(new Error("Failed to complete OAuth handshake with QQ. " + err.message),
                    {response: err.response});
   }
 
   return response.content;
 };
 
-Wechat.retrieveCredential = function(credentialToken, credentialSecret) {
+QQ.retrieveCredential = function(credentialToken, credentialSecret) {
   return Oauth.retrieveCredential(credentialToken, credentialSecret);
 };
