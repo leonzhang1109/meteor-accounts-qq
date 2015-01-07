9f99ce102d62557c709bd127fa9f6c130431bdb4
leonzhang1109 <leonzhang1109@hotmail.com>
2015-01-07 10:07:51 -0800

    Update and rename wechat_client.js to qq_client.js

--- qq_client.js
+++ qq_client.js
@@ -1,32 +1,32 @@
-Wechat = {};
+QQ = {};
 
-Wechat.requestCredential = function (options, credentialRequestCompleteCallback) {
+QQ.requestCredential = function (options, credentialRequestCompleteCallback) {
   if (!credentialRequestCompleteCallback && typeof options === 'function') {
     credentialRequestCompleteCallback = options;
     options = {};
   }
 
-  var config = ServiceConfiguration.configurations.findOne({service: 'wechat'});
+  var config = ServiceConfiguration.configurations.findOne({service: 'qq'});
   if (!config) {
     credentialRequestCompleteCallback && credentialRequestCompleteCallback(
       new ServiceConfiguration.ConfigError());
     return;
   }
   var credentialToken = Random.secret();
-  var loginStyle = OAuth._loginStyle('wechat', config, options);
+  var loginStyle = OAuth._loginStyle('qq', config, options);
   var scope = (options && options.requestPermissions) || ['basic', 'likes', 'relationships', 'comments'];
   var flatScope = _.map(scope, encodeURIComponent).join('+');
 
   var loginUrl =
-    'https://open.weixin.qq.com/connect/oauth2/authorize' +
-      '?appid=' + config.appId +
+    'https://graph.qq.com/oauth2.0/authorize' +
+      '?client_id=' + config.clientId +
       '&response_type=code' +
       '&scope=' + flatScope +
-      '&redirect_uri=' + OAuth._redirectUri('wechat', config) +
+      '&redirect_uri=' + OAuth._redirectUri('qq', config) +
       '&state=' + OAuth._stateParam(loginStyle, credentialToken);
 
   OAuth.launchLogin({
-    loginService: "wechat"
+    loginService: "qq"
     , loginStyle: loginStyle
     , loginUrl: loginUrl
     , credentialRequestCompleteCallback: credentialRequestCompleteCallback
